06a2a3180f92ffa0ae1aff038767bcb58246ae3a
kuche1 <znamsiparolata4esno@abv.bg>
2025-06-15 12:26:52 +0300

    update

--- src/handle_client.rs
+++ src/handle_client.rs
@@ -1,8 +1,10 @@
 use socket2::{Domain, SockAddr, Socket, Type}; // cargo add socket2
+use std::io;
 use std::io::{Read, Write};
 use std::net::{Ipv4Addr, SocketAddrV4, TcpStream};
+use std::thread;
 
-pub fn main(mut stream: TcpStream, ip_translated: Ipv4Addr, remote_port: u16) {
+pub fn main(mut client_stream: TcpStream, ip_translated: Ipv4Addr, remote_port: u16) {
     // // echo server
     // let mut buffer = [0; 512];
     // let n = stream.read(&mut buffer).unwrap(); // TODO get rid of the `unwrap`
@@ -27,9 +29,40 @@ pub fn main(mut stream: TcpStream, ip_translated: Ipv4Addr, remote_port: u16) {
     }
 
     if let Err(e) = socket.connect(&SockAddr::from(remote_addr)) {
-        eprintln!("could not connect to remote host -> {}", e);
+        eprintln!("could not connect to remote host {} -> {}", remote_addr, e);
         return;
     }
 
-    let remote_stream: TcpStream = socket.into();
+    let mut remote_stream: TcpStream = socket.into();
+
+    ////// TODO generated code
+
+    let mut client_stream_clone = match client_stream.try_clone() {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!("could not clone client_stream -> {}", e);
+            return;
+        }
+    };
+    let mut remote_stream_clone = match remote_stream.try_clone() {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!("could not clone remote_stream -> {}", e);
+            return;
+        }
+    };
+
+    // forward: client -> remote
+    let client_to_remote = thread::spawn(move || {
+        io::copy(&mut client_stream, &mut remote_stream).ok();
+    });
+
+    // forward: remote -> client
+    let remote_to_client = thread::spawn(move || {
+        io::copy(&mut remote_stream_clone, &mut client_stream_clone).ok();
+    });
+
+    // wait for either direction to finish
+    let _ = client_to_remote.join();
+    let _ = remote_to_client.join();
 }
